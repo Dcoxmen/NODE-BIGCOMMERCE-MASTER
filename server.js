27adb350c0788cd4421031f0b53423246c632e5e
Chadtao206 <chenchen408@gmail.com>
2021-09-25 16:03:35 -0700

    working data calls

--- server.js
+++ server.js
@@ -1,55 +1,48 @@
-require('dotenv').config();
-const expressHbs = require('express-handlebars')
-const express = require('express')
-const path = require('path')
-const cors = require('cors')
-const mongoose = require('mongoose');
-const connectDB = require('./config/db')
+require("dotenv").config();
+const expressHbs = require("express-handlebars");
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+const cors = require("cors");
+const mongoose = require("mongoose");
+// const connectDB = require('./config/db')
 
 const app = express();
 
 //Connect DB
-connectDB()
-
-
+// connectDB()
 
 app.use(cors());
-app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-//Handlebars Helpers
-const {
-    truncate,
-    select
-} = require('./helpers/hbs')
+// Handlebars Helpers
+const { truncate, select } = require("./helpers/hbs");
 
-
-app.engine('handlebars', expressHbs({
+app.engine(
+  "handlebars",
+  expressHbs({
     helpers: {
-    truncate: truncate,
-    select: select
-},
-defaultLayout: 'main'}))
-app.set('view engine', 'handlebars')
-app.set('views', 'views')
+      truncate: truncate,
+      select: select,
+    },
+    defaultLayout: "main",
+  })
+);
+app.set("view engine", "handlebars");
+app.set("views", "views");
 
+const indexRoutes = require("./routes/index");
+const productsRoutes = require("./routes/products");
 
+// Set static folder
+app.use(express.static(path.join(__dirname, "public")));
 
-const indexRoutes = require('./routes/index')
-const productsRoutes = require('./routes/products')
+app.use("/", indexRoutes);
+app.use("/products", productsRoutes);
 
-//Set static folder
-app.use(express.static(path.join(__dirname, 'public')))
+const PORT = process.env.PORT || 4000;
 
-
-
-
-app.use('/',indexRoutes)
-app.use('/products',productsRoutes)
-
-
-const PORT = process.env.PORT || 4000
-
-app.listen(PORT, () => console.log(`Express Server Now Running On localhost ${PORT}`))
-
-
+app.listen(PORT, () =>
+  console.log(`Express Server Now Running On localhost ${PORT}`)
+);
